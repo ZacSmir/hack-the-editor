c1e7a228beff73100ca178034dca094b04ffe0a6
ZacSmir <Zach@smirin.us>
2016-12-02 08:43:46 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var editor = $(parent.document.body)
 var topbar = $(parent.parent.document.body).find("#topbar")
 
-editor.find("div").css({font:"comic sans"})
+editor.find("div").css({background:"aquamarine"})
 
 var allDivsInTheEditor = editor.find("div")
 
